c673285656e8d41ef288794b7f8d9661759f3b1a
zan <104793677+zanprgmr@users.noreply.github.com>
2024-10-17 21:49:52 +0200

    1.3 update
    Check releases tab for detailed info

--- VoxelLauncher/main.py
+++ VoxelLauncher/main.py
@@ -1,45 +1,51 @@
+import _tkinter
 import pickle
 import shlex
-import threading
 import tkinter.messagebox as messagebox
 import os
 from webbrowser import open as webopen
+import ctypes
+import logging
 
 import customtkinter as ctk
 import minecraft_launcher_lib as mc
 import requests
-from PIL import Image
 
 import utils
 
-appdata_path = os.path.join(os.getenv('APPDATA'), 'VoxelLauncher')
-os.makedirs(appdata_path, exist_ok=True)
-config_file = os.path.join(appdata_path, "config.pickle")
-
 shared_data = {"jvmArguments": None}
+windows_instances = []
 
 
 def apply_shared_data(key, value):
     shared_data[key] = value
+    logging.info(f"{key} applied")
 
 
-def settings_window(version_drop):
+# Function that launches the desired window and adds its instance into
+# an array, for it to be accessed universally
+def launch_windows(window=0, version_drop=None):
+    # 1 -> Settings window
+    # 2 -> Installer window
+    # 3 -> Utilities
+    if window == 1:
+        windows_instances.append(settings_window())
+    elif window == 2:
+        windows_instances.append(installer_window())
+    elif window == 3:
+        windows_instances.append(utility_window(version_drop))
+
+
+def settings_window():
+    # Gets the JVM arguments, and saves them into the settings file
     def split_args_and_send(key, value):
         f_value = shlex.split(value)
         apply_shared_data(key, f_value)
         sett_window.destroy()
 
-    def repair_async():
-        def repair_version():
-            mc.install.install_minecraft_version(version_drop.get(), utils.get_dir())
-            messagebox.showinfo("Version repaired", f"The version: {version_drop.get()} has been repaired successfully")
-
-        install_thread = threading.Thread(target=repair_version)
-        install_thread.start()
-
     sett_window = ctk.CTkToplevel()
     sett_window.title("Settings")
-    sett_window.geometry("200x130")
+    sett_window.geometry("200x100")
     sett_window.lift()
     sett_window.attributes('-topmost', True)
     sett_window.after_idle(sett_window.attributes, '-topmost', False)
@@ -50,54 +56,57 @@ def settings_window(version_drop):
     jvm_args = ctk.CTkEntry(sett_window, placeholder_text="Enter jvm arguments")
     jvm_args.grid(row=1, column=0, padx=(30, 0))
 
-    repair_ver_btn = ctk.CTkButton(sett_window, text="Repair version", command=repair_async)
-    repair_ver_btn.grid(row=3, column=0, padx=(30, 0), pady=(5, 0))
-
+    # Updates the label if arguments are in the config file
     if shared_data["jvmArguments"]:
         jvm_args.insert(0, shared_data["jvmArguments"])
 
-    apply_bt = ctk.CTkButton(
-        sett_window,
-        text="Apply",
-        command=lambda: split_args_and_send("jvmArguments", jvm_args.get())
-    )
+    apply_bt = ctk.CTkButton(sett_window, text="Apply",
+                             command=lambda: split_args_and_send("jvmArguments", jvm_args.get()))
     apply_bt.grid(row=2, column=0, padx=(30, 0), pady=(5, 0))
 
     return sett_window
 
 
 def installer_window():
+    # Updates the version label whenever "Show snapshots" is pressed
     def update_versions():
         show_snapshots = snap_check.get()
-        installer_drop.configure(values=utils.return_downloadable_versions(show_snapshots))
+        logging.info("Updating versions available")
+        installer_drop.configure(values=utils.MinecraftLauncher.return_downloadable_versions(show_snapshots))
 
     def install_version():
         def install_and_notify():
             if fabric_check.get() == 0 and forge_check.get() == 0:
-                mc.install.install_minecraft_version(installer_drop.get(), utils.get_dir())
+                mc.install.install_minecraft_version(installer_drop.get(), utils.MinecraftLauncher.get_dir())
                 messagebox.showinfo("Installation Complete", f"Minecraft {installer_drop.get()}"
                                                              f" has been installed successfully!")
-            elif fabric_check.get() == 1 and forge_check.get() == 1:
-                fabric_check.deselect()
-                forge_check.deselect()
+            # Install fabric
             elif fabric_check.get() == 1 and forge_check.get() == 0:
                 try:
-                    mc.fabric.install_fabric(installer_drop.get(), utils.get_dir())
+                    mc.fabric.install_fabric(installer_drop.get(), utils.MinecraftLauncher.get_dir())
                     messagebox.showinfo("Installation Complete", f"Minecraft {installer_drop.get()}"
                                                                  f" fabric has been installed successfully!")
                 except Exception as e:
                     messagebox.showinfo("Error", f"Minecraft {installer_drop.get()}"
                                                  f" Fabric could not be installed: {e}")
+            # Install forge
             elif fabric_check.get() == 0 and forge_check.get() == 1:
                 forge_ver = mc.forge.find_forge_version(installer_drop.get())
                 if not forge_ver:
                     messagebox.showinfo("No forge version", f"Minecraft {installer_drop.get()}"
                                                             f" is not compatible with Forge")
                 else:
-                    mc.forge.install_forge_version(forge_ver, utils.get_dir())
+                    mc.forge.install_forge_version(forge_ver, utils.MinecraftLauncher.get_dir())
+                    messagebox.showinfo("Installation Complete", f"Minecraft {installer_drop.get()}"
+                                                                 f" forge has been installed successfully!")
+            elif fabric_check.get() == 1 and forge_check.get() == 1:
+                fabric_check.deselect()
+                forge_check.deselect()
+            logging.info("Installed selected version")
+            update_versions()
 
-        install_thread = threading.Thread(target=install_and_notify)
-        install_thread.start()
+
+        utils.ThreadingUtils.run_in_thread(install_and_notify)
 
     inst_window = ctk.CTkToplevel()
     inst_window.title("Installation window")
@@ -115,7 +124,8 @@ def installer_window():
     forge_check = ctk.CTkCheckBox(inst_window, text="Install Forge")
     forge_check.grid(row=3, column=0, pady=(4, 0), padx=(35, 0), sticky="w")
 
-    installer_drop = ctk.CTkComboBox(inst_window, values=utils.return_downloadable_versions(snap_check.get()))
+    installer_drop = ctk.CTkComboBox(inst_window,
+                                     values=utils.MinecraftLauncher.return_downloadable_versions(snap_check.get()))
     installer_drop.grid(row=0, column=0, padx=25, pady=8)
 
     install_ver_bt = ctk.CTkButton(inst_window, text="Install Version", command=install_version)
@@ -124,15 +134,51 @@ def installer_window():
     return inst_window
 
 
+def utility_window(version_drop):
+    utility_win = ctk.CTkToplevel()
+    utility_win.title("Utilities")
+    utility_win.geometry("200x200")
+    utility_win.attributes('-topmost', True)
+    utility_win.after_idle(utility_win.attributes, '-topmost', False)
+    utility_win.after(50, utility_win.lift)
+
+    backup_bt = ctk.CTkButton(utility_win, text="Backup worlds into desktop", width=190,
+                              command=utils.MinecraftLauncher.backup_worlds)
+    backup_bt.grid(row=0, column=0, pady=(10, 0), padx=(5, 0))
+
+    repair_ver_btn = ctk.CTkButton(utility_win, text="Repair version", width=190,
+                                   command=lambda: utils.MinecraftLauncher.repair_version(version_drop))
+    repair_ver_btn.grid(row=1, column=0, padx=(5, 0), pady=(5, 0))
+
+    cfg_label = ctk.CTkLabel(utility_win, text="Manage minecraft configuration")
+    cfg_label.grid(row=2, column=0, pady=(10, 0))
+
+    export_cfg_bt = ctk.CTkButton(utility_win, text="Export", width=95,
+                                  command=utils.MinecraftLauncher.export_mc_config)
+    export_cfg_bt.grid(row=3, column=0, padx=(5, 0), sticky="w")
+    import_cfg_bt = ctk.CTkButton(utility_win, text="Import", width=95,
+                                  command=utils.MinecraftLauncher.import_mc_config)
+    import_cfg_bt.grid(row=3, column=0, sticky="e")
+
+    install_mr_bt = ctk.CTkButton(utility_win, text="Install .mrpack", width=190,
+                                  command=utils.MinecraftLauncher.install_mrpack)
+    install_mr_bt.grid(row=4, column=0, pady=(15, 0), padx=(5, 0))
+    return utility_win
+
+
+# Function that parses the news data from the api and adds it to the frame on the main window
 def append_news(news_frame, news_data):
     def open_article(url):
         webopen(url)
 
-    for news in news_data["entries"]:
+    # Index means the number of news shown
+    # Changing it to a number greater than 30 may cause the launcher to be slower
+    for index, news in enumerate(news_data["entries"]):
+        if index >= 20:
+            break
         title = news["title"]
         subtitle = news["text"]
         article_url = news["readMoreLink"]
-
 
         title_label = ctk.CTkLabel(news_frame, text=title, font=("Arial", 16, "bold"), wraplength=320)
         title_label.pack()
@@ -148,11 +194,17 @@ def append_news(news_frame, news_data):
         spacer.pack()
 
 
-
-
-
 def main_window():
-    utils.create_file_if_not_exists(config_file)
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+    config_file = utils.FileManagement.get_config_dir()
+    utils.FileManagement.create_file_if_not_exists(config_file)
+
     ctk.set_default_color_theme("green")
     ctk.set_appearance_mode("dark")
 
@@ -160,56 +212,76 @@ def main_window():
     app.title("VoxelLauncher")
     app.geometry("610x300")
 
+    # Icons (Window and Taskbar)
+    appid = "zanprgmr.VoxelLauncher.Launcher.app"
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(appid)
+    try:
+        app.iconbitmap("assets/pris-marine.ico")
+
+    except FileNotFoundError:
+        app.iconbitmap(utils.FileManagement.pyinstaller_find("pris-marine.ico"))
+    except _tkinter.TclError:
+        pass  # Unknown error that may pop on the .exe bundle
+
+
+
     app.resizable(False, False)
     app.grid_rowconfigure(0, weight=1)
 
-    try:
-        folder_img = Image.open("assets/folder.png")
-    except FileNotFoundError:
-        folder_img = Image.open(utils.pyinstaller_find("folder.png"))
 
-    try:
-        gear_img = Image.open("assets/gear.png")
-    except FileNotFoundError:
-        gear_img = Image.open(utils.pyinstaller_find("gear.png"))
+    folder_img = utils.FileManagement.load_asset("folder.png")
+    gear_img = utils.FileManagement.load_asset("gear.png")
+    light_img = utils.FileManagement.load_asset("light.png")
 
+    logging.info("Assets and properties loaded")
 
-    launch_bt = ctk.CTkButton(app, text="Launch", command=lambda: utils.execute_mc(app, name_tx, version_drop,
-                                                                                   installer_window, settings_window,
-                                                                                   shared_data, config_file), width=200,
+    launch_bt = ctk.CTkButton(app, text="Launch",
+                              command=lambda: utils.MinecraftLauncher.execute_mc(app, name_tx, version_drop,
+                                                                                 shared_data, config_file,
+                                                                                 windows_instances),
+                              width=200,
                               height=60, font=(None, 20))
     launch_bt.grid(row=0, column=0, pady=(10, 0), padx=(10, 0))
 
-    version_drop = ctk.CTkComboBox(app, values=utils.return_installed_versions(), width=200)
+    version_drop = ctk.CTkComboBox(app, values=utils.MinecraftLauncher.return_installed_versions(), width=200)
     version_drop.grid(row=1, column=0, padx=(10, 0), pady=(10, 0))
 
     name_tx = ctk.CTkEntry(app, placeholder_text="Username", width=120)
     name_tx.grid(row=2, column=0, sticky="w", padx=(10, 0), pady=(120, 0))
 
-    sett_window_button = ctk.CTkButton(app, text="", image=ctk.CTkImage(gear_img),
-                                       command=lambda: settings_window(version_drop), width=35)
-    sett_window_button.grid(row=2, column=0, pady=(120, 0), padx=(95, 0))
+    sett_window_bt = ctk.CTkButton(app, text="", image=ctk.CTkImage(gear_img),
+                                   command=lambda: launch_windows(1), width=35)
+    sett_window_bt.grid(row=2, column=0, pady=(120, 0), padx=(95, 0))
 
     open_dir_bt = ctk.CTkButton(app, text="", image=ctk.CTkImage(folder_img), width=35,
-                                command=lambda: os.startfile(utils.get_dir()))
+                                command=lambda: os.startfile(utils.MinecraftLauncher.get_dir()))
     open_dir_bt.grid(row=2, column=0, sticky="e", pady=(120, 0))
 
-    install_win_bt = ctk.CTkButton(app, text="Installations window", command=installer_window, width=200)
-    install_win_bt.grid(row=3, column=0, padx=(10, 0), pady=(10, 10))
+    install_win_bt = ctk.CTkButton(app, text="Installations window", command=lambda: launch_windows(2), width=160)
+    install_win_bt.grid(row=3, column=0, padx=(10, 0), pady=(10, 10), sticky="w")
+
+    utility_win_bt = ctk.CTkButton(app, text="", image=ctk.CTkImage(light_img), width=35,
+                                   command=lambda: launch_windows(3, version_drop))
+    utility_win_bt.grid(row=3, column=0, sticky="w", padx=(174, 0))
 
     scrollable_frame = ctk.CTkScrollableFrame(app, width=360)
-    scrollable_frame.grid(row=0, column=1, rowspan=4, sticky="nsew", padx=(10, 10), pady=(10, 10))
+    scrollable_frame.grid(row=0, column=2, rowspan=4, sticky="nsew", padx=(10, 10), pady=(10, 10))
+
+    logging.info("Graphic elements loaded")
 
     try:
 
         news_data = requests.get("https://launchercontent.mojang.com/news.json").json()
+        # 0.10s faster but may cause errors if the window is closed too early (Testing)
+        # threading.Thread(target=lambda:  append_news(scrollable_frame, news_data)).start()
         append_news(scrollable_frame, news_data)
-    except requests.RequestException:
+        logging.info("News loaded")
+
+    except requests.RequestException as e:
         def offline_msg():
-            messagebox.showwarning("No internet", "Could not connect to internet, some functions like installing or "
-                                                  "repairing a version are not available."
-                                                  "\nCustom versions could not be available neither.")
-            threading.Thread(target=offline_msg).start()
+            messagebox.showwarning("No internet", f"Could not connect to the internet: {e}")
+            utils.ThreadingUtils.run_in_thread(offline_msg)
+            logging.info("No internet")
 
     try:
         with open(config_file, "rb") as config_read:
@@ -218,16 +290,13 @@ def main_window():
             version_drop.set(config[1])
             if config[2]:
                 shared_data.update(config[2])
-    except (TypeError, EOFError, FileNotFoundError):
+        logging.info("Config loaded")
+    except (TypeError, EOFError, FileNotFoundError) as e:
+        logging.error(f"Config NOT LOADED, error: {e}")
         pass
-
 
     return app
 
 
-def main():
-    main_window().mainloop()
-
-
 if __name__ == "__main__":
-    main()
+    main_window().mainloop()
--- VoxelLauncher/utils.py
+++ VoxelLauncher/utils.py
@@ -1,94 +1,215 @@
+import _tkinter
 import os
+import platform
+import shutil
 import sys
 import tkinter.filedialog
-import minecraft_launcher_lib as mc
 from os.path import exists
+from pathlib import Path
 from pickle import dump
 from subprocess import run
+import tkinter.simpledialog
+import minecraft_launcher_lib as mc
+import tkinter
 from tkinter import messagebox
+from threading import Thread
+from PIL import Image
+from typing import List
+import logging
 
 
-def get_dir(custom_dir=False):
-    if custom_dir:
-        dir = tkinter.filedialog.askdirectory()
-    else:
-        dir = mc.utils.get_minecraft_directory()
-
-    return dir
+logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
 
 
-def return_downloadable_versions(show_snapshots=False):
-    ver_list = []
-    if show_snapshots:
-        for ver in mc.utils.get_version_list():
-            ver_list.append(ver["id"])
-    else:
-        for ver in mc.utils.get_version_list():
-            if ver["type"] == "release":
-                ver_list.append(ver["id"])
-
-    return ver_list
-
-
-def return_installed_versions():
-    ver_list = []
-    for ver in mc.utils.get_installed_versions(get_dir()):
-        ver_list.append(ver["id"])
-
-    return ver_list
-
-
-def create_file_if_not_exists(filename):
-    if not exists(filename):
+class FileManagement:
+    @staticmethod
+    def pyinstaller_find(relative_path: str) -> str:
+        # Helper to find a file in a PyInstaller bundle
         try:
-            with open(filename, 'x') as file:
-                pass
-        except FileExistsError:
-            pass
+            base_path = sys._MEIPASS
+        except Exception:
+            base_path = os.path.abspath(".")
+        return os.path.join(base_path, relative_path)
+
+    @staticmethod
+    def get_config_dir() -> str:
+        # Return the directory where configuration files are stored based on OS
+        if platform.system() == 'Windows':
+            appdata_path = os.path.join(os.getenv('APPDATA'), 'VoxelLauncher')
+        elif platform.system() == 'Darwin':  # macOS
+            appdata_path = os.path.join(os.getenv('HOME'), 'Library', 'Application Support', 'VoxelLauncher')
+        else:
+            appdata_path = os.path.join(os.getenv('HOME'), '.config', 'VoxelLauncher')
+        os.makedirs(appdata_path, exist_ok=True)
+
+        return os.path.join(appdata_path, "config.pickle")
+
+    @staticmethod
+    def create_file_if_not_exists(filename: str):
+        # Create a file if it doesn't already exist
+        if not exists(filename):
+            logging.info(f"File created at {filename}")
+            Path(filename).touch()
+
+    @staticmethod
+    def save_config(config_file: str, name: tkinter.StringVar, version: tkinter.StringVar, shared_data: dict):
+        # Save user configuration using pickle
+        with open(config_file, "wb") as config_read:
+            dump([name.get(), version.get(), shared_data], config_read)
+        logging.info("Config saved")
+
+    @staticmethod
+    def load_asset(file_name):
+        try:
+            return Image.open(f"assets/{file_name}")
+        except FileNotFoundError:
+            return Image.open(FileManagement.pyinstaller_find(file_name))
 
 
-def save_config(config_file, name, version, shared_data):
-    with open(config_file, "wb") as config_read:
-        dump([name.get(), version.get(), shared_data], config_read)
+class MinecraftLauncher:
+    @staticmethod
+    def get_dir(custom_dir: bool = False) -> str:
+        # Get Minecraft directory or prompt user for a custom directory
+        return tkinter.filedialog.askdirectory() if custom_dir else mc.utils.get_minecraft_directory()
 
+    @staticmethod
+    def execute_mc(app: tkinter.Tk, name_tx, version_drop,
+                   shared_data: dict, config_file: str, windows: List[tkinter.Toplevel]):
+        # Execute Minecraft with user settings
+        logging.info("Executing minecraft")
+        if not name_tx.get():
+            messagebox.showwarning("Error", "Please enter your Minecraft name.")
+            name_tx.focus_force()
+            return
 
+        options = {
+            "username": name_tx.get(),
+            "uuid": "59ac9ba1-2c3c-4c99-b7ba-7258525068b4",
+            "token": "token",
+            "jvmArguments": shared_data["jvmArguments"],
+            "launcherName": "Voxel Launcher",
+            "launcherVersion": "1.3"
+        }
 
-def execute_mc(app, name_tx, version_drop, inst_window, sett_window, shared_data, config_file):
+        FileManagement.save_config(config_file, name_tx, version_drop, shared_data)
+        minecraft_command = mc.command.get_minecraft_command(version_drop.get(), MinecraftLauncher.get_dir(), options)
 
-    options = {
-    "username": name_tx.get(),
-    "uuid": "59ac9ba1-2c3c-4c99-b7ba-7258525068b4",
-    "token": "token",
-    "jvmArguments": shared_data["jvmArguments"],
-    "launcherName": "Voxel Launcher",
-    "launcherVersion": "1.2"
-}
-
-    if name_tx.get() == "":
-        messagebox.showwarning("Error", "Please enter your Minecraft name.")
-        name_tx.focus_force()
-    else:
-        save_config(config_file, name_tx, version_drop, shared_data)
-        minecraft_command = mc.command.get_minecraft_command(version_drop.get(), get_dir(), options)
-        if "inst_window" in globals() and inst_window.winfo_exists():
-            inst_window.withdraw()
-        if "sett_window" in globals() and sett_window.winfo_exists():
-            sett_window.withdraw()
+        # Hide all windows
         app.withdraw()
-        extract_path = f"get_dir()/versions/{version_drop.get()}/natives"
-        mc.natives.extract_natives(version_drop.get(), get_dir(), extract_path)
+        for window in windows:
+            try:
+                window.withdraw()
+            except _tkinter.TclError:
+                pass
+        logging.info("Windows hidden")
+
+
+        logging.info("Mc command executed")
+        # Run Minecraft
         run(minecraft_command)
-        if "inst_window" in globals() and inst_window.winfo_exists():
-            inst_window.deiconify()
-        if "sett_window" in globals() and sett_window.winfo_exists():
-            sett_window.deiconify()
+
+        # Restore windows
         app.deiconify()
+        for window in windows:
+            try:
+                window.deiconify()
+            except _tkinter.TclError:
+                pass
+        logging.info("Windows restored")
 
 
-# Compatibility with pyinstaller
-def pyinstaller_find(relative_path):
-    try:
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-    return os.path.join(base_path, relative_path)
+    @staticmethod
+    def return_downloadable_versions(show_snapshots):
+        # Return a list of available Minecraft versions, optionally including snapshots
+        versions = mc.utils.get_version_list()
+        return [ver["id"] for ver in versions if show_snapshots or ver["type"] == "release"]
+
+
+    @staticmethod
+    def return_installed_versions() -> List[str]:
+        # Return a list of installed Minecraft versions
+        versions = mc.utils.get_installed_versions(MinecraftLauncher.get_dir())
+        return [ver["id"] for ver in versions]
+
+
+    @staticmethod
+    def repair_version(version_drop: tkinter.StringVar):
+        # Repair selected Minecraft version
+
+        def repair_logic():
+            mc.install.install_minecraft_version(version_drop.get(), MinecraftLauncher.get_dir())
+            messagebox.showinfo("Version repaired", f"Version {version_drop.get()} has been repaired successfully")
+
+        logging.info(f"Starting version repairing, ver: {version_drop.get()}")
+        ThreadingUtils.run_in_thread(repair_logic)
+
+
+
+    @staticmethod
+    def backup_worlds():
+        # Backup Minecraft worlds
+
+        def backup_funct():
+            messagebox.showinfo("Important",
+                                "This process can take from 1-10 min depending on your worlds and their file size")
+            home_dir = Path.home()
+            worlds_dir = os.path.join(MinecraftLauncher.get_dir(), "saves")
+            zip_file = home_dir / "Desktop" / "minecraft_worlds_backup.zip"
+            logging.info("Starting world backup")
+            shutil.make_archive(str(zip_file).replace('.zip', ''), 'zip', worlds_dir)
+            messagebox.showinfo("Completed", f"Worlds backed up at {zip_file}")
+            logging.info("World backup finished")
+
+        ThreadingUtils.run_in_thread(backup_funct)
+
+    @staticmethod
+    def export_mc_config():
+        # Export Minecraft configuration to the desktop
+        desktop_path = Path.home() / "Desktop"
+        shutil.copy2(os.path.join(MinecraftLauncher.get_dir(), "options.txt"), desktop_path)
+        messagebox.showinfo("Configuration saved", f"Saved at {desktop_path}")
+
+    @staticmethod
+    def import_mc_config():
+        # Import Minecraft configuration from a file
+        src_file = tkinter.filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+        shutil.copy2(src_file, os.path.join(MinecraftLauncher.get_dir(), "options.txt"))
+        messagebox.showinfo("Done", "Configuration imported successfully")
+
+    @staticmethod
+    def install_mrpack():
+        # Install a .mrpack modpack
+
+        def install_mrpack_funct():
+            if modpack_directory:
+                mc.mrpack.install_mrpack(pack_dir, MinecraftLauncher.get_dir(), modpack_directory=modpack_directory)
+            else:
+                mc.mrpack.install_mrpack(pack_dir, MinecraftLauncher.get_dir())
+
+            mrpack_info = mc.mrpack.get_mrpack_information(pack_dir)
+            tkinter.Tk().after(0, lambda: messagebox.showinfo(f"{mrpack_info['name']} installed",
+                                                              f"Successfully installed: {mrpack_info['name']}\n"
+                                                              f"Minecraft Version: {mrpack_info['minecraftVersion']}"))
+
+        pack_dir = tkinter.filedialog.askopenfilename(filetypes=[("Modpack file", "*.mrpack")])
+
+        if not pack_dir:
+            messagebox.showwarning("No File Selected", "No modpack file was selected.")
+            return
+
+        modpack_directory = tkinter.filedialog.askdirectory() if messagebox.askyesno("Install Location",
+                                                                                     "Do you want to install the modpack into a specific directory?") else None
+
+        ThreadingUtils.run_in_thread(install_mrpack_funct)
+
+
+class ThreadingUtils:
+    @staticmethod
+    def run_in_thread(func):
+        logging.info(f"Threading function: {func}")
+        thread = Thread(target=func)
+        thread.start()
